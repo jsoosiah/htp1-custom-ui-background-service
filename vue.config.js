33767b6f24402a4ac463716f2666ec8e0233a946
jsoosiah <5963819+jsoosiah@users.noreply.github.com>
2021-06-16 22:52:29 -0400

    move build options to vue config

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,12 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       preload: 'src/preload.js',
+      builderOptions: {
+        appId: 'com.jsoosiah.htp1CustomUiBackgroundService',
+        electronDownload: {
+          cache: './.electron-cache',
+        },
+      },
     },
   },
 };
