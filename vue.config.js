ea7b93d37426938de4b0006ec6d31c289deb3353
jsoosiah <5963819+jsoosiah@users.noreply.github.com>
2021-06-17 16:06:13 -0400

    remove win 32bit

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,7 @@ module.exports = {
         win: {
           target: {
             target: 'nsis',
-            arch: ['x64', 'ia32'],
+            arch: ['x64'],
           },
         },
         nsis: {},
